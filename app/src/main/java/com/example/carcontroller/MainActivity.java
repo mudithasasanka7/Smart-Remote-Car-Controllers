e4781e786f995a0e42e1722add6b1dcb4172a6ce
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2024-12-05 15:18:32 +0530

    updating

--- app/src/main/java/com/example/carcontroller/MainActivity.java
+++ app/src/main/java/com/example/carcontroller/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.carcontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView joystickHandle;
+    private RelativeLayout joystickBase;
+    private float baseCenterX, baseCenterY;
+    private float baseRadius, handleRadius;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
